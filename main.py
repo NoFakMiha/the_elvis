480c448a52b3b5dd29bc840fcb3809d1b3405d4b
Miha Novak <miha.novak@outlook.de>
2022-09-11 10:16:44 +0200

    made simpler commands close, new, delete, check

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class TheMainConsole:
 
     def showing_the_table(self, data):
         extracting_data = data
-        print("Here i am")
         table = Table(title="Your list of projects")
         table.add_column("Created", justify="left", style="cyan", no_wrap=True)
         table.add_column("Title", justify="left", style="magenta")
@@ -40,28 +39,28 @@ def run_it():
     running_in_project = True
     while  running:
         user_decision = input("Chose the project: ")
-
-        if "close main" in user_decision:  # shut down the program
+        # commands
+        if "close" in user_decision:  # shut down the program
             running = False
 
-        if "new project" in user_decision:  # creating new project
+        if "new" in user_decision:  # creating new project
             json_data.adding_project(title=user_decision.split("new")[1],
                                      creation_time=now.strftime("%m.%d.%y | %H:%M:%S"))
             main_console.showing_the_table(data=main_console.loading_json_data())
 
-        if "check project" in user_decision:  # checking the  project
+        if "check" in user_decision:  # checking the  project
             while running_in_project:
                 user_decision_in_the_project = input("Chose the task:")
 
-                if "new task" in user_decision_in_the_project:
+                if "new" in user_decision_in_the_project:
                     pass
 
 
-                if "close project" in user_decision_in_the_project:
+                if "close" in user_decision_in_the_project:
                     main_console.showing_the_table(data=main_console.loading_json_data())
                     running_in_project = False
 
-        if "delete project" in user_decision:
+        if "delete" in user_decision:
             pass
 
 
